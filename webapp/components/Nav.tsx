b32f8b161154b20d6eb6b8783c4cc9aa9dbc274b
aa85192 <151709477+aa85192@users.noreply.github.com>
2026-04-07 21:16:16 +0800

    Update Nav.tsx

--- webapp/components/Nav.tsx
+++ webapp/components/Nav.tsx
@@ -22,7 +22,7 @@ export default function Nav() {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0z" />
               </svg>
             </div>
-            <span className="font-semibold text-gray-900 text-sm">放貸管理</span>
+            <span className="font-semibold text-gray-900 text-sm">WTK的放貸管理</span>
           </div>
 
           {/* Links */}
